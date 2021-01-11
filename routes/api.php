9ddc6ecb887330c6a75e12b973de21576e29ee7c
Your Name <you@example.com>
2021-01-11 18:47:58 +0000

    Api_curd_rest

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resources([
+    'categories' => App\Http\Controllers\CategoryController::class,
+    'products' =>  App\Http\Controllers\ProductController::class,
+]);
+
