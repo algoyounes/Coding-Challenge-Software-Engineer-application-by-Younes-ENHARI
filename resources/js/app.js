9d1ea1dc7cf2c48799468cb2c96121c0fe94a4be
Your Name <you@example.com>
2021-01-11 19:32:09 +0000

    Front_routes_edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,7 @@ Vue.use(IconsPlugin)
 
 
 const routes = [
+    { path: '/', name: 'product_list', component: require('./components/Product/ListComponent.vue').default },
     { path: '/product/add', name: 'product_add', component: require('./components/Product/FormComponent.vue').default },
     { path: '/product/edit/:id', name: 'product_edit', component: require('./components/Product/FormComponent.vue').default, props: true },
     { path: '/category', name: 'category_list', component: require('./components/Category/ListComponent.vue').default },
