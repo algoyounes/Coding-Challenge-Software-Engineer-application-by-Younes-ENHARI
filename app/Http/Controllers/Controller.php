4e7f091f32b6ed66abca42873f761679be469696
Your Name <you@example.com>
2021-02-13 11:57:15 +0000

    update_Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,7 +20,7 @@ class Controller extends BaseController
      *
      * @return int
      */
-    public function getStatusCode()
+    public function getStatusCode(): int
     {
         return $this->statusCode;
     }
@@ -31,7 +31,7 @@ class Controller extends BaseController
      * @param int $statusCode Value to set
      * @return self
      */
-    public function setStatusCode($statusCode)
+    public function setStatusCode($statusCode): self
     {
         $this->statusCode = $statusCode;
         return $this;
@@ -43,7 +43,7 @@ class Controller extends BaseController
      * @param array $data
      * @return JsonResponse
      */
-    protected function respondWithArray(array $data)
+    protected function respondWithArray(array $data): JsonResponse
     {
         return new JsonResponse($data, $this->statusCode, [ 'Content-Type' => 'application/json' ]);
     }
@@ -54,7 +54,7 @@ class Controller extends BaseController
      * @param array $message
      * @return JsonResponse
      */
-    protected function respondWithError(array $message)
+    protected function respondWithError(array $message): JsonResponse
     {
         return $this->respondWithArray($message);
     }
@@ -65,7 +65,7 @@ class Controller extends BaseController
      * @param array $item
      * @return JsonResponse
      */
-    protected function respondWithItem(array $item)
+    protected function respondWithItem(array $item): JsonResponse
     {
         return $this->setStatusCode(201)->respondWithArray($item);
     }
@@ -76,7 +76,7 @@ class Controller extends BaseController
      * @param string $message
      * @return JsonResponse
      */
-    public function errorNotFound($message)
+    public function errorNotFound($message): JsonResponse
     {
         return $this->setStatusCode(404)->respondWithError([ "error" => $message ]);
     }
